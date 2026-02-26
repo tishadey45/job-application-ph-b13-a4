86bfb9def8003b2dd459389c85ea6e99325a5017
Tisha Dey <tishadey2025@outlook.com>
2026-02-26 16:49:37 +0600

    :green_heart: rejected button done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const interviewButton = document.querySelectorAll(".interview-button");
-// const rejectedButton = document.querySelectorAll(".rejected-button");
+const rejectedButton = document.querySelectorAll(".rejected-button");
+
 // console.log(interviewButton);
 interviewButton.forEach((button) => {
   button.addEventListener("click", (event) => {
-    console.log(event.target);
+    // console.log(event.target);
     // alert("Congratulations! You have been selected for an interview. Please check your email for further details.");
     const jobApply = event.target.closest(".job-apply");
     // console.log({jobApply,event});
@@ -30,7 +31,42 @@ interviewButton.forEach((button) => {
                                     <span class="salary">${salary}</span>
                                 </p>
                                 <p class="description text-gray-600 mt-4">${description}</p>`;
-     const interviewTab = document.getElementById("interview-tab");
-     interviewTab.prepend(interview);                           
+    const interviewTab = document.getElementById("interview-tab");
+    interviewTab.prepend(interview);
+  });
+});
+
+
+rejectedButton.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // console.log(event.target);
+    // alert("We regret to inform you that your application has been rejected. We encourage you to apply for other opportunities in the future.");
+     const jobApply = event.target.closest(".job-apply");
+    // console.log({jobApply,event});
+    const companyName = jobApply.querySelector(".company-name").textContent;
+    const position = jobApply.querySelector(".position").textContent;
+    // console.log({companyName,position});
+    const location = jobApply.querySelector(".location").textContent;
+    // console.log({companyName,location});
+    const jobType = jobApply.querySelector(".job-type").textContent;
+    // console.log({jobType});
+    const salary = jobApply.querySelector(".salary").textContent;
+    const description = jobApply.querySelector(".description").textContent;
+    // console.log({companyName,position,location,jobType,salary,description});
+    const rejected = document.createElement("div");
+    rejected.className = `card job-apply bg-white p-6 rounded-xl mb-6 shadow-sm`;
+    rejected.innerHTML = `  <div class="flex justify-between">
+                                    <h3 class="company-name text-lg font-semibold">${companyName}</h3>
+                                    <i class="fa-regular fa-trash-can"></i>
+                                </div>
+                                <p class="position text-gray-600">${position}</p>
+                                <p class="job-meta text-sm text-gray-500 mt-2">
+                                    <span class="location">${location}</span> •
+                                    <span class="job-type">${jobType}</span> •
+                                    <span class="salary">${salary}</span>
+                                </p>
+                                <p class="description text-gray-600 mt-4">${description}</p>`;
+    const rejectedTab = document.getElementById("rejected-tab");
+    rejectedTab.prepend(rejected);
   });
 });
